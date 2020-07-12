8d380e8f04947e703caa68336b1352d25dd1331c
FIRST_NAME LAST_NAMEgit config user.email MY_NAME@example.com <parniczky.aron@gmail.com>
2020-07-12 19:31:01 +0200

    style change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
    production: false,
-   API_URL: 'http://api.openweathermap.org/data/2.5',
-   API_KEY: '69fc5903875165ce620a666a99b42349'
+   API_URL: null,
+   API_KEY: null
 };
