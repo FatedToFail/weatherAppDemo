6da9ccd1ea6aa20b1d60430ddc48b1cdf7b4a890
FIRST_NAME LAST_NAMEgit config user.email MY_NAME@example.com <parniczky.aron@gmail.com>
2020-07-13 00:15:33 +0200

    fix animations

--- src/app/forecast-modal/forecast-modal.component.ts
+++ src/app/forecast-modal/forecast-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import {
   animate, state, style,
   trigger, transition,
@@ -23,12 +23,53 @@ import ForecastData from '../forecastData';
         animate('.21s ease-in')
       ]),
     ]),
+    trigger('openCloseMobile', [
+      state('open', style({
+        transform: 'translateY(calc(100vh - 75px))'
+      })),
+      state('openActive', style({
+        transform: 'translateY(0)'
+      })),
+      state('closed', style({
+        transform: 'translateY(100vh)'
+      })),
+      transition('open => closed', [
+        animate('.13s ease-out')
+      ]),
+      transition('open => openActive', [
+        animate('.21s ease-in')
+      ]),
+      transition('openActive => open', [
+        animate('.13s ease-out')
+      ]),
+      transition('openActive => close', [
+        animate('.17s ease-out')
+      ]),
+      transition('closed => open', [
+        animate('.21s ease-in')
+      ]),
+    ]),
   ],
   templateUrl: './forecast-modal.component.html',
   styleUrls: ['./forecast-modal.component.scss']
 })
-export class ForecastModalComponent {
+export class ForecastModalComponent implements OnChanges {
   @Input() isVisilbe: boolean;
   @Input() forecasts: ForecastData[];
   @Input() city: string;
+  isActive: boolean;
+
+  public setIsActive(): void {
+    this.isActive = !this.isActive;
+  }
+
+  public getState(): string {
+    if (!this.isVisilbe) return 'closed';
+    if (this.isVisilbe && !this.isActive) return 'open';
+    return 'openActive';
+  }
+
+  public ngOnChanges(data): void {
+    console.log(data);
+  }
 }
